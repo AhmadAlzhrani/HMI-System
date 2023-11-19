e29a101b58118e66d31df1ddc0b06ad26fd4c7ed
Roxel069 <86969414+Roxel069@users.noreply.github.com>
2023-11-19 19:41:42 +0300

    created sidebar and icons

--- HMI/frontend/src/App.js
+++ HMI/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react'
+import {FaPencilAlt} from 'react-icons/fa'
 
 function App() {
 
@@ -22,45 +23,59 @@ function App() {
             body: JSON.stringify(dataToSend),
         });
     };
-    
-    // send the code with the button
-    //sendData();
+
+    /*const SideBar = () =>{
+        return(
+            <div>
+                <div className = 'logo'>
+                    <img style={{width:'10%', height: '10%'} } 
+                    src = 'https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403' 
+                    alt = 'cato'/>
+                </div>
+                <div className = 'ingr'>
+                    {(typeof backendData.ingredients === 'undefined') ?  (<p>loading..</p>):(
+                        backendData.ingredients.map((user, index) => ( 
+                            <ul>
+                                <li key={index}>{user}</li>
+                            </ul>
+                        ))
+                    )}
+                </div>
+                <div>
+                    <CustomButton text="Send" onClick={sendData} />
+                </div>
+            </div>
+        )
+    }*/
 
     return(
-        <>
-        <div class = 'ingr'>
-            {(typeof backendData.ingredients === 'undefined') ?  (
-                <p>loading..</p>
-            ):(
-               backendData.ingredients.map((user, index) => ( 
-                     <p key={index}>{user}</p>
-                ))
-            )}
+        <div className='flex'>
+            <SideBar/>
         </div>
-        <div class = 'sendOrder'>
-            <button onClick={sendData}>Send</button>
-        </div>
-        <div class = 'sideBar'>alo</div>
-        </>
 
     )
 }
-/*function sideBar(){
+
+function CustomButton(props) {
+    return <button className='bg-orange-400 text-white font-medium px-4 py-1 rounded hover:bg-orange-500' onClick={props.onClick}>{props.text}</button>;
+}
+
+function SideBar(){
     return(
-        <div class = 'sideBar'>
-            <div class = 'logo'>
-                <img src = 'https://www.flaticon.com/svg/static/icons/svg/2991/2991144.svg' alt = 'logo'/>
-            </div>
-            <div class = 'menu'>
-                <ul>
-                    <li>Home</li>
-                    <li>Menu</li>
-                    <li>Order</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
+        <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-600 text-white shadow-lg'>
+           <SideBarIcon icon={<FaPencilAlt size='30'/>}/>
+        </div>
+
+    )
+}
+
+function SideBarIcon({icon}) {
+    return(
+        <div className='sidebar-icon'>
+            {icon}
         </div>
     )
-}*/
+}
+
 
 export default App;
